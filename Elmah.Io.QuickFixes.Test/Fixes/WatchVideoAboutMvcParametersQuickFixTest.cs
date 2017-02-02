f9c8bef5ab6112dcfd0c1ed1546adcaceb2738ba
Thomas Ardal <thomasardal@gmail.com>
2017-02-02 11:37:31 +0100

    Added quick fix with link to video tutorial on mvc parameter issue

---
+++ Elmah.Io.QuickFixes.Test/Fixes/WatchVideoAboutMvcParametersQuickFixTest.cs
@@ -0,0 +1,26 @@
+﻿using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class WatchVideoAboutMvcParametersQuickFixTest
+    {
+        [TestCase(true, "The parameters dictionary contains a null entry for parameter 'logId' of non-nullable type 'System.Guid' for method 'System.Web.Mvc.ActionResult Settings(System.Guid)' in 'Elmah.Io.Web.Controllers.ErrorLogController'. An optional parameter must be a reference type, a nullable type, or be declared as an optional parameter.Parameter name: parameters")]
+        [TestCase(false, null)]
+        [TestCase(false, "")]
+        [TestCase(false, "nothing valid here")]
+        public void CanFix(bool canFix, string title)
+        {
+            var quickFix = new WatchVideoAboutMvcParametersQuickFix();
+            var message = new Message { Title = title };
+            var actual = quickFix.CanFix(message);
+            Assert.That(actual, Is.EqualTo(canFix));
+            if (canFix)
+            {
+                var decorated = quickFix.Decorate(message);
+                Assert.That(decorated.Text, Is.EqualTo("ASP.NET MVC Actions and Parameters video tutorial"));
+                Assert.That(decorated.Url.ToString(), Is.EqualTo("https://app.pluralsight.com/player?author=scott-allen&name=mvc4-building-m2-controllers&mode=live&clip=2&course=mvc4-building"));
+            }
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/WatchVideoAboutMvcParametersQuickFix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class WatchVideoAboutMvcParametersQuickFix : QuickFixBase
+    {
+        public WatchVideoAboutMvcParametersQuickFix()
+        {
+            Icon = "fa-play";
+            Text = "ASP.NET MVC Actions and Parameters video tutorial";
+            Url = new Uri("https://app.pluralsight.com/player?author=scott-allen&name=mvc4-building-m2-controllers&mode=live&clip=2&course=mvc4-building");
+        }
+
+        public override bool CanFix(Message message)
+        {
+            if (string.IsNullOrWhiteSpace(message.Title)) return false;
+            return
+                message.Title.ToLower().Contains("the parameters dictionary contains a null entry for parameter") &&
+                message.Title.ToLower()
+                    .Contains(
+                        "an optional parameter must be a reference type, a nullable type, or be declared as an optional parameter");
+        }
+    }
+}
