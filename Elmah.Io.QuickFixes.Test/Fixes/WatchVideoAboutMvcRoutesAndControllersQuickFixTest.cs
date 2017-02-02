7624db53fcb6eb3f7af8e3a07900c26b154a80fd
Thomas Ardal <thomasardal@gmail.com>
2017-02-02 11:44:41 +0100

    Added quick fix with link to video tutorial on mvc routing issue

---
+++ Elmah.Io.QuickFixes.Test/Fixes/WatchVideoAboutMvcRoutesAndControllersQuickFixTest.cs
@@ -0,0 +1,26 @@
+﻿using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class WatchVideoAboutMvcRoutesAndControllersQuickFixTest
+    {
+        [TestCase(true, "The controller for path '/logging-to-elmah-io-from-microsoft-extensions-logging' was not found or does not implement IController.")]
+        [TestCase(false, null)]
+        [TestCase(false, "")]
+        [TestCase(false, "nothing valid here")]
+        public void CanFix(bool canFix, string title)
+        {
+            var quickFix = new WatchVideoAboutMvcRoutesAndControllersQuickFix();
+            var message = new Message { Title = title };
+            var actual = quickFix.CanFix(message);
+            Assert.That(actual, Is.EqualTo(canFix));
+            if (canFix)
+            {
+                var decorated = quickFix.Decorate(message);
+                Assert.That(decorated.Text, Is.EqualTo("ASP.NET MVC Routes and Controllers video tutorial"));
+                Assert.That(decorated.Url.ToString(), Is.EqualTo("https://app.pluralsight.com/player?author=scott-allen&name=mvc4-building-m2-controllers&mode=live&clip=1&course=mvc4-building"));
+            }
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/WatchVideoAboutMvcRoutesAndControllersQuickFix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class WatchVideoAboutMvcRoutesAndControllersQuickFix : QuickFixBase
+    {
+        public WatchVideoAboutMvcRoutesAndControllersQuickFix()
+        {
+            Icon = "fa-play";
+            Text = "ASP.NET MVC Routes and Controllers video tutorial";
+            Url = new Uri("https://app.pluralsight.com/player?author=scott-allen&name=mvc4-building-m2-controllers&mode=live&clip=1&course=mvc4-building");
+        }
+
+        public override bool CanFix(Message message)
+        {
+            if (string.IsNullOrWhiteSpace(message.Title)) return false;
+            return
+                message.Title.ToLower().Contains("the controller for path") &&
+                message.Title.ToLower().Contains("was not found or does not implement icontroller");
+        }
+    }
+}
