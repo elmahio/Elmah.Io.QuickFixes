adc33cf564f72feb11d9a932f62458001e8a1591
Thomas Ardal <thomasardal@gmail.com>
2017-03-12 14:40:54 +0100

    Added missing link in umbraco quick fix

--- Elmah.Io.QuickFixes/Fixes/UmbracoReadRazorDocumentationQuickFix.cs
+++ Elmah.Io.QuickFixes/Fixes/UmbracoReadRazorDocumentationQuickFix.cs
@@ -1,11 +1,14 @@
-﻿namespace Elmah.Io.QuickFixes.Fixes
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes
 {
     public class UmbracoReadRazorDocumentationQuickFix : QuickFixBase
     {
         public UmbracoReadRazorDocumentationQuickFix()
         {
             Icon = "fa-book";
-            Text = "Troubleshoot Razor errors";
+            Text = "Learn about Razor in Umbraco";
+            Url = new Uri("https://our.umbraco.org/documentation/Reference/Templating/Mvc/");
         }
 
         public override bool CanFix(Message message)
