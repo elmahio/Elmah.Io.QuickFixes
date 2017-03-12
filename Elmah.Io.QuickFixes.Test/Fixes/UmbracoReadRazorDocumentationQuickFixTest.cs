1442635f15f3136b4ed548dfad4b0923e92f0ccb
Thomas Ardal <thomasardal@gmail.com>
2017-03-12 12:17:04 +0100

    Troubleshoot Umbraco razor error

---
+++ Elmah.Io.QuickFixes.Test/Fixes/UmbracoReadRazorDocumentationQuickFixTest.cs
@@ -0,0 +1,21 @@
+﻿using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class UmbracoReadRazorDocumentationQuickFixTest
+    {
+        [TestCase(true, "System.Web.HttpCompileException", "in Umbraco.Core.Profiling.ProfilingView.Render")]
+        [TestCase(false, "System.Web.HttpCompileException", "in Profiling.ProfilingView.Render")]
+        [TestCase(false, "System.Web.HttpException", "in Umbraco.Core.Profiling.ProfilingView.Render")]
+        [TestCase(false, "System.Web.HttpCompileException", null)]
+        [TestCase(false, null, "in Umbraco.Core.Profiling.ProfilingView.Render")]
+        public void CanFixByUserAgent(bool expected, string type, string detail)
+        {
+            var quickFix = new UmbracoReadRazorDocumentationQuickFix();
+            var message = new Message { Type = type, Detail = detail };
+            var actual = quickFix.CanFix(message);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/UmbracoReadRazorDocumentationQuickFix.cs
@@ -0,0 +1,19 @@
+﻿namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class UmbracoReadRazorDocumentationQuickFix : QuickFixBase
+    {
+        public UmbracoReadRazorDocumentationQuickFix()
+        {
+            Icon = "fa-book";
+            Text = "Troubleshoot Razor errors";
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return !string.IsNullOrWhiteSpace(message.Type)
+                   && !string.IsNullOrWhiteSpace(message.Detail)
+                   && message.Type.Equals("System.Web.HttpCompileException")
+                   && message.Detail.Contains("Umbraco.Core.Profiling.ProfilingView.Render");
+        }
+    }
+}
