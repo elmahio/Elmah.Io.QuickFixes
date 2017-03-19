5aec0425519f6283c384da62e810581125818129
Thomas Ardal <thomasardal@gmail.com>
2017-03-19 19:48:56 +0100

    Quick fix with link to msdn documentation of all System* exception types

---
+++ Elmah.Io.QuickFixes.Test/Fixes/MsdnDocumentationQuickFixTest.cs
@@ -0,0 +1,27 @@
+﻿using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class MsdnDocumentationQuickFixTest
+    {
+        [TestCase(true, "System.NullReferenceException", "https://msdn.microsoft.com/en-us/library/system.nullreferenceexception.aspx")]
+        [TestCase(false, null, "")]
+        [TestCase(false, "", "")]
+        [TestCase(false, "nothing valid here", "")]
+        [TestCase(false, "Elmah.Io.CustomException", "")]
+        public void CanFixByUserAgent(bool canFix, string type, string url)
+        {
+            var quickFix = new MsdnDocumentationQuickFix();
+            var message = new Message { Type = type };
+            var actual = quickFix.CanFix(message);
+            Assert.That(actual, Is.EqualTo(canFix));
+            if (canFix)
+            {
+                var decorated = quickFix.Decorate(message);
+                Assert.That(decorated.Text, Is.EqualTo($"Documentation for {type}"));
+                Assert.That(decorated.Url.ToString(), Is.EqualTo(url));
+            }
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/MsdnDocumentationQuickFix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class MsdnDocumentationQuickFix : QuickFixBase
+    {
+        public MsdnDocumentationQuickFix()
+        {
+            Icon = "fa-book";
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return !string.IsNullOrWhiteSpace(message.Type) && message.Type.ToLower().StartsWith("system.");
+        }
+
+        public override QuickFixBase Decorate(Message message)
+        {
+            Url = new Uri($"https://msdn.microsoft.com/en-us/library/{message.Type.ToLower()}.aspx");
+            Text = $"Documentation for {message.Type}";
+            return this;
+        }
+    }
+}
