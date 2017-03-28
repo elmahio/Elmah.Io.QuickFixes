5a17ea1d8712fe9c675f3f9f5021a6340d2b47bd
Thomas Ardal <thomasardal@gmail.com>
2017-03-28 08:16:29 +0200

    Umbraco quick fix of corrupt cache

---
+++ Elmah.Io.QuickFixes.Test/Fixes/Umbraco/UmbracoCorruptCacheQuickFixTest.cs
@@ -0,0 +1,19 @@
+﻿using Elmah.Io.QuickFixes.Fixes.Umbraco;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes.Umbraco
+{
+    public class UmbracoCorruptCacheQuickFixTest
+    {
+        [TestCase(false, null)]
+        [TestCase(false, "")]
+        [TestCase(false, "Some string")]
+        [TestCase(true, "The Xml cache is corrupt. Use the Health Check data integrity dashboard to fix it.")]
+        public void CanFix(bool expectedCanFix, string title)
+        {
+            var quickFix = new UmbracoCorruptCacheQuickFix();
+            var canFix = quickFix.CanFix(new Message { Title = title });
+            Assert.That(canFix, Is.EqualTo(expectedCanFix));
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/Umbraco/UmbracoCorruptCacheQuickFix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes.Umbraco
+{
+    public class UmbracoCorruptCacheQuickFix : QuickFixBase
+    {
+        public UmbracoCorruptCacheQuickFix()
+        {
+            Icon = "fa-book";
+            Text = "Troubleshoot corrupt cache";
+            Url = new Uri("https://our.umbraco.org/forum/umbraco-7/using-umbraco-7/73079-xml-cache-data-integrity-check-in-730-what-is-it-and-why-does-the-fix-throw-an-exception");
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return
+                !string.IsNullOrWhiteSpace(message.Title) &&
+                message.Title.ToLower().StartsWith("the xml cache is corrupt. use the health check data integrity dashboard to fix it");
+        }
+    }
+}
