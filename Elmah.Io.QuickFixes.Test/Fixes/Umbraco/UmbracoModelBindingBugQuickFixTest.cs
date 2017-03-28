86ba8fbf5deb41ac1dd776a1fed2aab091dd2886
Thomas Ardal <thomasardal@gmail.com>
2017-03-28 08:07:00 +0200

    Umbraco quick fix of model binding issue

---
+++ Elmah.Io.QuickFixes.Test/Fixes/Umbraco/UmbracoModelBindingBugQuickFixTest.cs
@@ -0,0 +1,19 @@
+﻿using Elmah.Io.QuickFixes.Fixes.Umbraco;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes.Umbraco
+{
+    public class UmbracoModelBindingBugQuickFixTest
+    {
+        [TestCase(false, null)]
+        [TestCase(false, "")]
+        [TestCase(false, "Some string")]
+        [TestCase(true, "Cannot bind source type Umbraco.Web.Models.RenderModel to model type Application.BusinessLogic.Models.MasterModel`1[[Application.BusinessLogic.Models.Login, Application.BusinessLogic]]")]
+        public void CanFix(bool expectedCanFix, string title)
+        {
+            var quickFix = new UmbracoModelBindingBugQuickFix();
+            var canFix = quickFix.CanFix(new Message { Title = title });
+            Assert.That(canFix, Is.EqualTo(expectedCanFix));
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/Umbraco/UmbracoModelBindingBugQuickFix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes.Umbraco
+{
+    public class UmbracoModelBindingBugQuickFix : QuickFixBase
+    {
+        public UmbracoModelBindingBugQuickFix()
+        {
+            Icon = "fa-book";
+            Text = "Troubleshoot model binding issue";
+            Url = new Uri("https://our.umbraco.org/forum/extending-umbraco-and-using-the-api/76178-error-cannot-bind-source-type-type-to-model-type-type");
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return
+                !string.IsNullOrWhiteSpace(message.Title) &&
+                message.Title.ToLower().StartsWith("cannot bind source type umbraco.web.models.rendermodel to model type");
+        }
+    }
+}
