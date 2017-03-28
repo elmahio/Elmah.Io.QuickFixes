d0961744de67d338bc1db72353e14c870d008f1d
Thomas Ardal <thomasardal@gmail.com>
2017-03-28 08:12:59 +0200

    Umbraco quick fix showing custom controllers documentation

---
+++ Elmah.Io.QuickFixes.Test/Fixes/Umbraco/UmbracoReadCustomControllersDocumentationQuickFixTest.cs
@@ -0,0 +1,20 @@
+﻿using Elmah.Io.QuickFixes.Fixes.Umbraco;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes.Umbraco
+{
+    public class UmbracoReadCustomControllersDocumentationQuickFixTest
+    {
+        [TestCase(false, null)]
+        [TestCase(false, "")]
+        [TestCase(false, "Other string")]
+        [TestCase(true, "The current Document Type SearchResults matches a locally declared controller of type MyApp.Controllers.HomeController. Custom Controllers for Umbraco routing must implement 'Umbraco.Web.Mvc.IRenderController' and inherit from 'System.Web.Mvc.ControllerBase'.")]
+        public void CanFixByUserAgent(bool expected, string title)
+        {
+            var quickFix = new UmbracoReadCustomControllersDocumentationQuickFix();
+            var message = new Message { Title = title };
+            var actual = quickFix.CanFix(message);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/Umbraco/UmbracoReadCustomControllersDocumentationQuickFix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes.Umbraco
+{
+    public class UmbracoReadCustomControllersDocumentationQuickFix : QuickFixBase
+    {
+        public UmbracoReadCustomControllersDocumentationQuickFix()
+        {
+            Icon = "fa-book";
+            Text = "Learn about custom controllers in Umbraco";
+            Url = new Uri("https://our.umbraco.org/documentation/reference/routing/custom-controllers");
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return
+                !string.IsNullOrWhiteSpace(message.Title) &&
+                message.Title.ToLower().Contains("custom controllers for umbraco routing must implement");
+        }
+    }
+}
