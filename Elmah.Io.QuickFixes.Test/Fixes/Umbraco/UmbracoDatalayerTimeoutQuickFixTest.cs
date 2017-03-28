3b6d59705bde7008a5a004fdba897b43ad88b240
Thomas Ardal <thomasardal@gmail.com>
2017-03-28 08:22:18 +0200

    Umbraco quick fix of datalayer timeout

---
+++ Elmah.Io.QuickFixes.Test/Fixes/Umbraco/UmbracoDatalayerTimeoutQuickFixTest.cs
@@ -0,0 +1,21 @@
+﻿using Elmah.Io.QuickFixes.Fixes.Umbraco;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes.Umbraco
+{
+    public class UmbracoDatalayerTimeoutQuickFixTest
+    {
+        [TestCase(false, null, null)]
+        [TestCase(false, "", "")]
+        [TestCase(false, "", null)]
+        [TestCase(false, null, "")]
+        [TestCase(false, "Some string", "Some other string")]
+        [TestCase(true, "The wait operation timed out", "umbraco.datalayer.sqlhelper")]
+        public void CanFix(bool expectedCanFix, string title, string detail)
+        {
+            var quickFix = new UmbracoDatalayerTimeoutQuickFix();
+            var canFix = quickFix.CanFix(new Message { Title = title, Detail = detail });
+            Assert.That(canFix, Is.EqualTo(expectedCanFix));
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/Umbraco/UmbracoDatalayerTimeoutQuickFix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes.Umbraco
+{
+    public class UmbracoDatalayerTimeoutQuickFix : QuickFixBase
+    {
+        public UmbracoDatalayerTimeoutQuickFix()
+        {
+            Icon = "fa-book";
+            Text = "Troubleshoot timeout";
+            Url = new Uri("https://our.umbraco.org/projects/backoffice-extensions/falm-housekeeping/bugs-reports/42409-The-wait-operation-timed-out");
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return
+                !string.IsNullOrWhiteSpace(message.Title) &&
+                !string.IsNullOrWhiteSpace(message.Detail) &&
+                message.Title.ToLower().Contains("the wait operation timed out") &&
+                message.Detail.ToLower().Contains("umbraco.datalayer.sqlhelper");
+        }
+    }
+}
