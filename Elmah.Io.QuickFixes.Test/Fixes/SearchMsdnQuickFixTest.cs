f8331d6894022aee7357d19166cf551e5d7fbab4
Thomas Ardal <thomasardal@gmail.com>
2017-10-11 10:49:22 +0200

    Search msdn forums quick fix

---
+++ Elmah.Io.QuickFixes.Test/Fixes/SearchMsdnQuickFixTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class SearchMsdnQuickFixTest
+    {
+        [Test]
+        public void CanFix()
+        {
+            var quickFix = new SearchMsdnQuickFix();
+            var canFix = quickFix.CanFix(new Message());
+            Assert.That(canFix, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void CanDecorate()
+        {
+            var quickFix = new SearchMsdnQuickFix();
+            var decorated = quickFix.Decorate(new Message {Title = "This is a test"});
+            Assert.That(decorated.Url, Is.EqualTo(new Uri("https://social.msdn.microsoft.com/Forums/en-US/home?sort=relevancedesc&searchTerm=This is a test")));
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/SearchMsdnQuickFix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class SearchMsdnQuickFix : QuickFixBase
+    {
+        public SearchMsdnQuickFix()
+        {
+            Icon = "fa-windows";
+            Text = "Search MSDN Forums";
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return true;
+        }
+
+        public override QuickFixBase Decorate(Message message)
+        {
+            Url = new Uri("https://social.msdn.microsoft.com/Forums/en-US/home?sort=relevancedesc&searchTerm=" + message.Title);
+            return this;
+        }
+    }
+}
