fd2a13251cd79401a8f31552112488d1e2401d5b
Thomas Ardal <thomasardal@gmail.com>
2017-11-02 07:53:48 +0100

    SSL Certificate quick fix

---
+++ Elmah.Io.QuickFixes.Test/Fixes/CertificateQuickFixTest.cs
@@ -0,0 +1,20 @@
+﻿using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class CertificateQuickFixTest
+    {
+        [TestCase(true, "Could not establish trust relationship for the SSL/TLS secure channel")]
+        [TestCase(true, "could not establish trust relationship for the ssl/tls secure channel")]
+        [TestCase(true, "The underlying connection was closed: Could not establish trust relationship for the SSL/TLS secure channel.")]
+        [TestCase(false, "")]
+        [TestCase(false, "Some other message")]
+        [TestCase(false, null)]
+        public void CanFix(bool canFix, string detail)
+        {
+            var quickFix = new CertificateQuickFix();
+            Assert.That(quickFix.CanFix(new Message { Detail = detail }), Is.EqualTo(canFix));
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/CertificateQuickFix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class CertificateQuickFix : QuickFixBase
+    {
+        public CertificateQuickFix()
+        {
+            Icon = "fa-unlock-alt";
+            Url = new Uri("https://en.wikipedia.org/wiki/Certificate_authority");
+            Text = "Purchase a valid SSL certificate";
+        }
+
+        public override bool CanFix(Message message)
+        {
+            if (string.IsNullOrWhiteSpace(message.Detail)) return false;
+            return message
+                .Detail
+                .ToLower()
+                .Contains("could not establish trust relationship for the ssl/tls secure channel");
+        }
+    }
+}
