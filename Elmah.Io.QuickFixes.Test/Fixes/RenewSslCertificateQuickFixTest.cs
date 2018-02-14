b6ae569d9a448a198e4a5757ed7590d978d87dfa
Thomas Ardal <thomasardal@gmail.com>
2018-02-14 09:35:27 +0100

    Renew ssl certificate quick fix

---
+++ Elmah.Io.QuickFixes.Test/Fixes/RenewSslCertificateQuickFixTest.cs
@@ -0,0 +1,30 @@
+﻿using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class RenewSslCertificateQuickFixTest
+    {
+        [TestCase(true, "SSL Checker")]
+        [TestCase(false, null)]
+        [TestCase(false, "")]
+        [TestCase(false, "nothing valid here")]
+        public void CanFix(bool canFix, string source)
+        {
+            var quickFix = new RenewSslCertificateQuickFix();
+            var message = new Message { Source = source };
+            var actual = quickFix.CanFix(message);
+            Assert.That(actual, Is.EqualTo(canFix));
+            if (canFix)
+            {
+                Assert.That(quickFix.Text, Is.EqualTo("Renew SSL certificate"));
+                Assert.That(quickFix.Url.ToString(), Is.EqualTo("http://whichssl.com/comparisons/all-certificates.html"));
+            }
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/RenewSslCertificateQuickFix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class RenewSslCertificateQuickFix : QuickFixBase
+    {
+        public RenewSslCertificateQuickFix()
+        {
+            Icon = "fa-lock";
+            Url = new Uri("http://whichssl.com/comparisons/all-certificates.html");
+            Text = "Renew SSL certificate";
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return message?.Source?.Equals("SSL Checker") ?? false;
+        }
+    }
+}
