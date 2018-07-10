add7bf089e29613f06fe65c40dee429bedb7ca31
Thomas Ardal <thomasardal@gmail.com>
2018-07-10 15:08:19 +0200

    Added quick fix for dns issues

---
+++ Elmah.Io.QuickFixes.Test/Fixes/DnsIssuesQuickFixTest.cs
@@ -0,0 +1,18 @@
+﻿using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class DnsIssuesQuickFixTest
+    {
+        [TestCase(true, "The remote name could not be resolved: &#39;djdjdjdjdjksdjksdjksd.com&#39;")]
+        [TestCase(false, "Nothing to see here")]
+        [TestCase(false, null)]
+        [TestCase(false, "")]
+        public void CanFix(bool canFix, string detail)
+        {
+            var quickFix = new DnsIssuesQuickFix();
+            Assert.That(quickFix.CanFix(new Message { Detail = detail }), Is.EqualTo(canFix));
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/DnsIssuesQuickFix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class DnsIssuesQuickFix : QuickFixBase
+    {
+        public DnsIssuesQuickFix()
+        {
+            Icon = "fa-book";
+            Text = "Debugging DNS issues";
+            Url = new Uri("https://blog.elmah.io/debugging-system-net-webexception-the-remote-name-could-not-be-resolved/");
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return
+                !string.IsNullOrWhiteSpace(message.Detail) &&
+                message.Detail.ToLower().IndexOf("the remote name could not be resolved") != -1;
+        }
+    }
+}
