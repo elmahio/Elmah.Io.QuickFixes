7435fb9a041d938de1e4e0473f20c1ef1b45fa8f
Thomas Ardal <thomasardal@gmail.com>
2019-02-20 07:27:55 +0100

    Blog posts quick fix

---
+++ Elmah.Io.QuickFixes.Test/Fixes/BlogPostsQuickFixTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Elmah.Io.QuickFixes.Fixes;
+using NUnit.Framework;
+
+namespace Elmah.Io.QuickFixes.Test.Fixes
+{
+    public class BlogPostsQuickFixTest
+    {
+        [TestCase(true, "System.Net.WebException", "https://blog.elmah.io/debugging-system-net-webexception-the-remote-name-could-not-be-resolved/")]
+        [TestCase(true, "System.OutOfMemoryException", "https://blog.elmah.io/debugging-system-outofmemoryexception-using-net-tools/")]
+        [TestCase(false, null, "")]
+        [TestCase(false, "", "")]
+        [TestCase(false, "nothing valid here", "")]
+        public void CanFixByUserAgent(bool canFix, string type, string url)
+        {
+            var quickFix = new BlogPostsQuickFix();
+            var message = new Message { Type = type };
+            var actual = quickFix.CanFix(message);
+            Assert.That(actual, Is.EqualTo(canFix));
+            if (canFix)
+            {
+                var decorated = quickFix.Decorate(message);
+                Assert.That(decorated.Text, Is.EqualTo($"Blog post about {type}"));
+                Assert.That(decorated.Url.ToString(), Is.EqualTo(url));
+            }
+        }
+    }
+}
---
+++ Elmah.Io.QuickFixes/Fixes/BlogPostsQuickFix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elmah.Io.QuickFixes.Fixes
+{
+    public class BlogPostsQuickFix : QuickFixBase
+    {
+        private readonly Dictionary<string, Uri> _links;
+
+        public BlogPostsQuickFix()
+        {
+            _links = new Dictionary<string, Uri>
+            {
+                {"System.Net.WebException", new Uri("https://blog.elmah.io/debugging-system-net-webexception-the-remote-name-could-not-be-resolved/")},
+                {"System.OutOfMemoryException", new Uri("https://blog.elmah.io/debugging-system-outofmemoryexception-using-net-tools/")},
+            };
+            Icon = "fa-file-text-o";
+        }
+
+        public override bool CanFix(Message message)
+        {
+            return !string.IsNullOrWhiteSpace(message.Type) && _links.ContainsKey(message.Type);
+        }
+
+        public override QuickFixBase Decorate(Message message)
+        {
+            Url = _links[message.Type];
+            Text = string.Format("Blog post about {0}", message.Type);
+            return this;
+        }
+    }
+}
